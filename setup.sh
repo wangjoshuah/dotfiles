b37ce7dad724b7b65c3a7582767acb6b7df28fd8
Joshua Wang <joshwang@shwang-MBP-2013.local>
2020-11-10 23:08:31 -0600

    add asdf install to script

--- setup.sh
+++ setup.sh
@@ -8,3 +8,19 @@ set -ev
 # Install all brew oependencies
 brew bundle install
 
+# Add asdf to shell
+echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.bash_profile
+
+# add asdf autocomplete to shell
+echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.bash_profile
+
+# install asdf plugins
+## add node gpg key from https://github.com/asdf-vm/asdf-nodejs
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+asdf plugin add nodejs
+asdf plugin add poetry
+asdf plugin add python
+asdf plugin add yarn
+
+# link fish config
+ln -s .config/ ~/.config
